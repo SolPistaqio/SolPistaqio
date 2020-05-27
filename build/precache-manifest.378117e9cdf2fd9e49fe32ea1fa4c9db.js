1de0367116bd497d51c11572868f9d1b4934509f
SolPistaqio <37481224+SolPistaqio@users.noreply.github.com>
2020-05-27 14:56:27 +0300

    fixed dev

---
+++ build/precache-manifest.378117e9cdf2fd9e49fe32ea1fa4c9db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17df186a3018833a5fa19fc91dc3aae6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1fed5c66ed87c14e8cfc",
+    "url": "/static/css/main.c45076a6.chunk.css"
+  },
+  {
+    "revision": "34841005a6e8b82c973c",
+    "url": "/static/js/2.a297ce34.chunk.js"
+  },
+  {
+    "revision": "f2b2351c4f6998110bcbd70068a0d356",
+    "url": "/static/js/2.a297ce34.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1fed5c66ed87c14e8cfc",
+    "url": "/static/js/main.23837afc.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  }
+]);
